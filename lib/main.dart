ad511593b2dcc2c9f1737d29345d16896c2d87b4
Pascual28 <kuyagicafe@gmail.com>
2022-12-13 11:31:55 +0800

    New Update

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main(){
       '/second': (context) => SecondRoute(),
       '/third': (context) => const ThirdRoute(),
       '/fourth': (context) => const FourthRoute(),
-
+      '/fifth' : (context) => const LoginForm(),
     },
   )); //MaterialApp
 }
@@ -57,7 +57,7 @@ class _MainPageState extends StatelessWidget {
                           child: MaterialButton(
                               minWidth: double.infinity,
                               onPressed: () {
-                                Navigator.pushNamed(context, '/second');
+                                Navigator.pushNamed(context, '/fifth');
                               },
                               color: Colors.purpleAccent,
                               textColor: Colors.black,
@@ -73,7 +73,109 @@ class _MainPageState extends StatelessWidget {
       );
 }
 
+class LoginForm extends StatefulWidget {
+  const LoginForm({Key? key}) : super(key: key);
 
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+class _LoginFormState extends State<LoginForm> {
+  TextEditingController uname = TextEditingController();
+  TextEditingController upaswd = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Login Image
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                  width: 400,
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/secured.gif',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+
+            //Edit Text
+            SizedBox(height: 20.0),
+
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
+              child: TextField(
+                controller: uname,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'User Name',
+                    hintText: 'Please Enter User Name'),
+              ),
+            ),
+
+            SizedBox(height: 10.0),
+
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
+              child: TextField(
+                controller: upaswd,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Please Enter Password'),
+              ),
+            ),
+
+            SizedBox(height: 20.0),
+
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20.0)),
+              child: TextButton(
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+                onPressed: () {
+
+
+                  String _name = uname.text.toString().trim();
+                  String _paswd = upaswd.text.toString();
+
+                  if (_name.isEmpty) {
+                    print('Please Enter User Name');
+
+                  } else if (_paswd.isEmpty) {
+                    print('Please Enter Password');
+
+                  } else {
+                    if (_name == 'admin' && _paswd == 'admin') {
+                      Navigator.pushNamed(context, '/second');
+                    } else {
+                      print('Login Fail');
+                    }
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 // ignore: must_be_immutable
 class SecondRoute extends StatefulWidget {
